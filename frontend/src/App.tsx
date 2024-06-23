512087a2a777f88681fa7d9af70e2d35c1c82cdf
squilliam34 <wdfan0128@gmail.com>
2024-06-23 15:23:14 -0400

    deleted default. now includes homepage component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import "./css/App.css";
+import "./css/styles.css";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Homepage />
+    </div>
   );
 }
 
