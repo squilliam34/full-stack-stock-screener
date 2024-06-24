7aa288f64e22c4f5f47765dd9442a9e21f0c34e7
squilliam34 <wdfan0128@gmail.com>
2024-06-23 23:32:06 -0400

    added image in flex container with section

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -3,8 +3,11 @@ import TypedSection from "../../components/TypedSection";
 
 const Homepage: React.FC = () => {
   return (
-    <div>
+    <div className="flex small-margin-top">
       <TypedSection />
+      <div className="chart-container">
+        <img src="images/bull.png" />
+      </div>
     </div>
   );
 };
