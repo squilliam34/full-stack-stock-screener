5173187c22469b8774cbeff2fe30e9ea4ab03171
squilliam34 <wdfan0128@gmail.com>
2024-06-23 21:43:19 -0400

    name change

---
+++ frontend/src/components/TypedSection.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+const TypedSection: React.FC = () => {
+  const header = "Want an easy way to find stocks?";
+  const paragraph =
+    "I've got the solution for you. By signing up, you'll get access to a stock screener, a portfolio performance tracker, and recommendations for you based on your portfolio. I've done all the hard work for you, so make your life a little easier and take advantage of the all-in-one tool I've built.";
+
+  const [typeHeader] = useTypewriter({
+    words: [header],
+    typeSpeed: 20,
+  });
+
+  const [typeParagraph] = useTypewriter({
+    words: [paragraph],
+    typeSpeed: 25,
+  });
+
+  return (
+    <div className="section">
+      <h1 className="header typing-header">{typeHeader}</h1>
+      <p className="paragraph-body typing">{typeParagraph}</p>
+    </div>
+  );
+};
+
+export default TypedSection;
--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Section from "../../components/Section";
+import TypedSection from "../../components/TypedSection";
 
 const Homepage: React.FC = () => {
   return (
     <div>
-      <Section
-        header1="Want an easy way to find stocks?"
-        paragraph="I've got the solution for you. By signing up, you'll get access to a stock screener, a portfolio performance tracker, and recommendations for you based on your portfolio. I've done all the hard work for you, so make your life a little easier and take advantage of the all-in-one tool I've built."
-      />
+      <TypedSection />
     </div>
   );
 };
