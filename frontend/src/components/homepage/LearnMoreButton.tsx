ba722e22def3a68e74effe3995fa80dc66c929f5
squilliam34 <wdfan0128@gmail.com>
2024-06-24 23:42:08 -0400

    new component to scroll farther down page

---
+++ frontend/src/components/homepage/LearnMoreButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-scroll";
+import { FiChevronsDown } from "react-icons/fi";
+
+const LearnMoreButton: React.FC = () => {
+  return (
+    <div>
+      <Link to="learn-more" smooth={true} duration={500} offset={-300}>
+        <div className="learn-more-button flex center-align column">
+          <div>Click to Learn More</div>
+          <FiChevronsDown size={20} />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default LearnMoreButton;
