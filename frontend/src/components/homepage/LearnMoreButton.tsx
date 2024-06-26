7591e2ecd0b29d33803395383c66a665f844d508
squilliam34 <wdfan0128@gmail.com>
2024-06-25 23:41:56 -0400

    adjusted offset of scroll

--- frontend/src/components/homepage/LearnMoreButton.tsx
+++ frontend/src/components/homepage/LearnMoreButton.tsx
@@ -5,7 +5,7 @@ import { FiChevronsDown } from "react-icons/fi";
 const LearnMoreButton: React.FC = () => {
   return (
     <div>
-      <Link to="learn-more" smooth={true} duration={500} offset={-300}>
+      <Link to="learn-more" smooth={true} duration={500} offset={-100}>
         <div className="learn-more-button flex center-align column">
           <div>Click to Learn More</div>
           <FiChevronsDown size={20} />
