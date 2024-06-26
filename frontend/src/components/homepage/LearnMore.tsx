fda41b607c90eeac15f6d0f98bd876c8367e4924
squilliam34 <wdfan0128@gmail.com>
2024-06-26 00:02:01 -0400

    initial learn more component

---
+++ frontend/src/components/homepage/LearnMore.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const LearnMore: React.FC = () => {
+  return (
+    <div id="learn-more" className="learn-more">
+      <h1>
+        Welcome to my advanced stock screener and portfolio management platform!
+      </h1>
+      <p className="paragraph-body">
+        I've designed a website to empower investors with comprehensive tools
+        for informed decision-making. Whether you're a seasoned trader or just
+        beginning your investment journey, our website offers a seamless
+        experience tailored to your needs.
+      </p>
+      <div className="bullet-point">
+        Key Features:
+        <ul>
+          <li>
+            Stock Screener: Easily filter through thousands of stocks using
+            customizable criteria such as market cap, P/E ratio, dividend yield,
+            and more. Find the perfect investments that match your unique
+            strategy and goals.
+          </li>
+          <li>
+            Portfolio Tracker: Monitor the performance of your investments in
+            real-time. Track portfolio value, asset allocation, and historical
+            returns effortlessly. Gain actionable insights into your holdings'
+            performance over time.
+          </li>
+          <li>
+            Personalized Recommendations: Receive intelligent stock
+            recommendations based on your portfolio composition, risk tolerance,
+            and investment preferences. Discover new opportunities aligned with
+            your financial objectives.
+          </li>
+          <li>
+            User-Friendly Interface: Enjoy a sleek and intuitive interface that
+            simplifies complex financial data. Access charts, news updates, and
+            analyst ratings to stay informed and make confident decisions.
+          </li>
+          <li>
+            Secure Account Management: Sign in securely to access your
+            personalized dashboard, where your portfolio data and preferences
+            are safely stored and updated in real-time.
+          </li>
+        </ul>
+      </div>
+      <p className="paragraph-body">
+        Whether you're looking to build wealth over the long term or seize
+        short-term opportunities, our platform equips you with the tools and
+        insights needed to navigate the markets effectively. Start optimizing
+        your investment strategy today with our powerful stock screener and
+        portfolio management tools.
+      </p>
+    </div>
+  );
+};
+
+export default LearnMore;
