2c7b3fa43972871e7f50611eef362feff84499c0
squilliam34 <wdfan0128@gmail.com>
2024-06-28 15:35:46 -0400

    configurations to import awsExports

---
+++ frontend/src/types/aws-exports.d.ts
@@ -0,0 +1,15 @@
+declare module 'aws-exports' {
+    const config: {
+      aws_project_region: string;
+      aws_cognito_identity_pool_id: string;
+      aws_cognito_region: string;
+      aws_user_pools_id: string;
+      aws_user_pools_web_client_id: string;
+      oauth?: object;
+      aws_appsync_graphqlEndpoint?: string;
+      aws_appsync_region?: string;
+      aws_appsync_authenticationType?: string;
+    };
+    export default config;
+  }
+  
