d9579143e77df8269ad471626119a81faef3e8b3
squilliam34 <wdfan0128@gmail.com>
2024-07-01 18:35:07 -0400

    initial screener

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "./css/styles.css";
 import Homepage from "./pages/Homepage/Homepage";
 import Signup from "./pages/Signup/Signup";
+import Screener from "./pages/Screener/Screener";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/Global/Navbar";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/homepage" element={<Homepage />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/screener" element={<Screener />} />
         </Routes>
       </main>
     </div>
