874d0cd9f15bb23bca94c013fe22de135a5e90c7
squilliam34 <wdfan0128@gmail.com>
2024-07-03 23:52:18 -0400

    added functionality to get data from select elemnt

--- frontend/src/components/Screener/Category.tsx
+++ frontend/src/components/Screener/Category.tsx
@@ -3,13 +3,14 @@ import React from "react";
 interface CategoryProps {
   label: string;
   options: string[];
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Category: React.FC<CategoryProps> = ({ label, options }) => {
+const Category: React.FC<CategoryProps> = ({ label, options, onChange }) => {
   return (
     <div>
       <div className="category">{label}</div>
-      <select>
+      <select onChange={onChange}>
         <option value="">--Select--</option>
         {options.map((option, index) => (
           <option key={index} value={option}>
