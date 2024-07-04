a4ed647de4da1b4abf59933a33990a8cd0dc855c
squilliam34 <wdfan0128@gmail.com>
2024-07-04 00:47:25 -0400

    added screener instructions

--- frontend/src/pages/Screener/Screener.tsx
+++ frontend/src/pages/Screener/Screener.tsx
@@ -62,7 +62,24 @@ const Screener: React.FC = () => {
   };
 
   return (
-    <div className="flex">
+    <div className="flex screener-page">
+      <div className="results-container">
+        <h2>How to use the screener:</h2>
+        <ul>
+          <li>
+            Choose your criteria - Choose what characteristics you want your
+            desired equities to have. You can screen by sector, market cap,
+            dividend, !! insert more as you add them !! The world is your
+            oyster!
+          </li>
+          <li>
+            See your results - It's that easy! Just press the "See Results"
+            button and start browsing the different equities that fit your
+            criteria on the far right. There you'll see different equities as
+            well as some key characteristics about them.
+          </li>
+        </ul>
+      </div>
       <Filter
         onFilterChange={handleFilterChange}
         onSeeResults={handleSeeResults}
