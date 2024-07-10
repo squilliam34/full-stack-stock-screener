38e50f64c034b33a1098aa4fc8e0fb28d6578350
squilliam34 <wdfan0128@gmail.com>
2024-07-09 21:49:22 -0400

    functionality to close popup on click anywhere

--- frontend/src/components/Screener/InfoPopup.tsx
+++ frontend/src/components/Screener/InfoPopup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaRegCircleQuestion } from "react-icons/fa6";
 
 interface InfoPopupProps {
@@ -9,15 +9,42 @@ interface InfoPopupProps {
 const InfoPopup: React.FC<InfoPopupProps> = ({ title, info }) => {
   const [isVisible, setIsVisible] = useState(false);
 
-  const makeVisibile = () => {
+  const popupRef = useRef<HTMLDivElement>(null);
+  const iconRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      popupRef.current &&
+      !popupRef.current.contains(event.target as Node) &&
+      iconRef.current &&
+      !iconRef.current.contains(event.target as Node)
+    ) {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isVisible]);
+
+  const makeVisible = () => {
     setIsVisible(!isVisible);
   };
 
   return (
     <div>
-      <FaRegCircleQuestion className="popup-button" onClick={makeVisibile} />
+      <div ref={iconRef}>
+        <FaRegCircleQuestion className="popup-button" onClick={makeVisible} />
+      </div>
       {isVisible && (
-        <div className="popup-content">
+        <div className="popup-content" ref={popupRef}>
           <h2>{title}</h2>
           <p>{info}</p>
         </div>
