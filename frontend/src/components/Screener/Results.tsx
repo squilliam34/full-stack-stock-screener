a1a09e362a3fe4c667b91cd2228e82f46d8284ee
squilliam34 <wdfan0128@gmail.com>
2024-07-19 15:37:19 -0400

    imported result interface and removed old result

--- frontend/src/components/Screener/Results.tsx
+++ frontend/src/components/Screener/Results.tsx
@@ -1,38 +1,5 @@
 import React from "react";
-
-interface Result {
-  symbol: string;
-  sector: string;
-  marketcap: string;
-  dividend: number;
-  price: number;
-  peratio: number;
-  pegratio: number;
-  psratio: number;
-  pbratio: number;
-  currentratio: number;
-  sharperatio: number;
-  eps: number;
-  netincome: number;
-  totalrevenue: number;
-  beta: number;
-  ebitda: number;
-  roe: number;
-  grossmargin: number;
-  ttmhigh: number;
-  ttmlow: number;
-  ma10: number;
-  ma20: number;
-  ma30: number;
-  ma40: number;
-  ma50: number;
-  previousyearnetincome: number;
-  previousyeartotalrevenue: number;
-  previousyearebitda: number;
-  previousquarternetincome: number;
-  previousquartertotalrevenue: number;
-  previousquarterebitda: number;
-}
+import { Result } from "../../types/interfaces";
 
 interface ResultsProps {
   results: Result[];
--- frontend/src/pages/Screener/Screener.tsx
+++ frontend/src/pages/Screener/Screener.tsx
@@ -1,39 +1,6 @@
 import React, { useState } from "react";
+import { Result } from "../../types/interfaces";
 import { Filter, Results, BackToTheTop } from "../../components/Screener";
-
-interface Result {
-  symbol: string;
-  sector: string;
-  marketcap: string;
-  dividend: number;
-  price: number;
-  peratio: number;
-  pegratio: number;
-  psratio: number;
-  pbratio: number;
-  currentratio: number;
-  sharperatio: number;
-  eps: number;
-  netincome: number;
-  totalrevenue: number;
-  beta: number;
-  ebitda: number;
-  roe: number;
-  grossmargin: number;
-  ttmhigh: number;
-  ttmlow: number;
-  ma10: number;
-  ma20: number;
-  ma30: number;
-  ma40: number;
-  ma50: number;
-  previousyearnetincome: number;
-  previousyeartotalrevenue: number;
-  previousyearebitda: number;
-  previousquarternetincome: number;
-  previousquartertotalrevenue: number;
-  previousquarterebitda: number;
-}
 
 const Screener: React.FC = () => {
   const [filters, setFilters] = useState<{ [key: string]: string }>({});
