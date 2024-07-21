f007199af634096e55da426157bd00365855064e
squilliam34 <wdfan0128@gmail.com>
2024-07-21 00:15:44 -0400

    added stock chart placeholder to component

--- frontend/src/pages/StockDetails/StockDetails.tsx
+++ frontend/src/pages/StockDetails/StockDetails.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Result } from '../../types/interfaces';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { Result } from "../../types/interfaces";
+import { useParams } from "react-router-dom";
 
 interface StockDetailsProps {
   data: Result[];
@@ -15,40 +15,57 @@ const StockDetails: React.FC<StockDetailsProps> = ({ data }) => {
   }
 
   return (
-    <div className="stock-details">
-      <h2>{stock.symbol}</h2>
-      <ul>
-        <li>Sector: {stock.sector}</li>
-        <li>Market Cap: {stock.marketcap}</li>
-        <li>Price: ${stock.price}</li>
-        <li>Dividend Yield: {stock.dividend}%</li>
-        <li>P/E Ratio: {stock.peratio}</li>
-        <li>PEG Ratio: {stock.pegratio}</li>
-        <li>P/S Ratio: {stock.psratio}</li>
-        <li>P/B Ratio: {stock.pbratio}</li>
-        <li>Current Ratio: {stock.currentratio}</li>
-        <li>Sharpe Ratio: {stock.sharperatio}</li>
-        <li>EPS: {stock.eps}</li>
-        <li>Net Income: {stock.netincome}</li>
-        <li>Total Revenue: {stock.totalrevenue}</li>
-        <li>Beta: {stock.beta}</li>
-        <li>EBITDA: {stock.ebitda}</li>
-        <li>ROE: {stock.roe}</li>
-        <li>Gross Margin: {stock.grossmargin}%</li>
-        <li>TTM High: {stock.ttmhigh}</li>
-        <li>TTM Low: {stock.ttmlow}</li>
-        <li>MA10: {stock.ma10}</li>
-        <li>MA20: {stock.ma20}</li>
-        <li>MA30: {stock.ma30}</li>
-        <li>MA40: {stock.ma40}</li>
-        <li>MA50: {stock.ma50}</li>
-        <li>Previous Year Net Income: {stock.previousyearnetincome}</li>
-        <li>Previous Year Total Revenue: {stock.previousyeartotalrevenue}</li>
-        <li>Previous Year EBITDA: {stock.previousyearebitda}</li>
-        <li>Previous Quarter Net Income: {stock.previousquarternetincome}</li>
-        <li>Previous Quarter Total Revenue: {stock.previousquartertotalrevenue}</li>
-        <li>Previous Quarter EBITDA: {stock.previousquarterebitda}</li>
-      </ul>
+    <div className="flex">
+      <div className="stock-details">
+        <h2>{stock.symbol}</h2>
+        <ul>
+          <li>Sector: {stock.sector}</li>
+          <li>Market Cap: {stock.marketcap}</li>
+          <li>Price: ${stock.price}</li>
+          <li>Dividend Yield: {stock.dividend}%</li>
+          <li>P/E Ratio: {stock.peratio}</li>
+          <li>PEG Ratio: {stock.pegratio}</li>
+          <li>P/S Ratio: {stock.psratio}</li>
+          <li>P/B Ratio: {stock.pbratio}</li>
+          <li>Current Ratio: {stock.currentratio}</li>
+          <li>Sharpe Ratio: {stock.sharperatio}</li>
+          <li>EPS: {stock.eps}</li>
+          <li>Net Income: {stock.netincome}</li>
+          <li>Total Revenue: {stock.totalrevenue}</li>
+          <li>Beta: {stock.beta}</li>
+          <li>EBITDA: {stock.ebitda}</li>
+          <li>ROE: {stock.roe}</li>
+          <li>Gross Margin: {stock.grossmargin}%</li>
+          <li>TTM High: {stock.ttmhigh}</li>
+          <li>TTM Low: {stock.ttmlow}</li>
+          <li>MA10: {stock.ma10}</li>
+          <li>MA20: {stock.ma20}</li>
+          <li>MA30: {stock.ma30}</li>
+          <li>MA40: {stock.ma40}</li>
+          <li>MA50: {stock.ma50}</li>
+          <li>Previous Year Net Income: {stock.previousyearnetincome}</li>
+          <li>Previous Year Total Revenue: {stock.previousyeartotalrevenue}</li>
+          <li>Previous Year EBITDA: {stock.previousyearebitda}</li>
+          <li>Previous Quarter Net Income: {stock.previousquarternetincome}</li>
+          <li>
+            Previous Quarter Total Revenue: {stock.previousquartertotalrevenue}
+          </li>
+          <li>Previous Quarter EBITDA: {stock.previousquarterebitda}</li>
+        </ul>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          color: "white",
+          backgroundColor: "black",
+          width: "40rem",
+          marginTop: "5rem",
+          height: "25rem",
+          alignContent: "center",
+        }}
+      >
+        Chart goes here
+      </div>
     </div>
   );
 };
