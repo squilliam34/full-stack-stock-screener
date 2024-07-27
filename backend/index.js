b5cafb8d46e23abdc00400768ba8d1ca6d4cf9a9
squilliam34 <wdfan0128@gmail.com>
2024-07-27 01:01:01 -0400

    server setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const polygonRoutes = require('./routes/polygonRoutes')
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/polygon', polygonRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
