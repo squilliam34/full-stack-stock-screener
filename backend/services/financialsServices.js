1b9ddc10d30556a858c96adfebae7da7ec3862da
squilliam34 <wdfan0128@gmail.com>
2024-07-27 01:01:43 -0400

    service to get company financials

---
+++ backend/services/financialsServices.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+const API_KEY = process.env.POLYGON_API_KEY
+
+const getCompanyFinancials = async (ticker) => {
+  try {
+    const BASE_URL = `https://api.polygon.io/vX/reference/financials?ticker=${ticker}&apiKey=${API_KEY}`;
+    const response = await axios.get(BASE_URL);
+    return response.data;
+    }
+    catch (error) {
+        throw new Error(`Error fetching financials: ${error}`)
+    }
+}
+
+module.exports = {getCompanyFinancials}
