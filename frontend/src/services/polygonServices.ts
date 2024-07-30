c050e1b8209646b1bde31e3c210403f744f68c6d
squilliam34 <wdfan0128@gmail.com>
2024-07-30 00:10:34 -0400

    function to get moving average data in frontend

--- frontend/src/services/polygonServices.ts
+++ frontend/src/services/polygonServices.ts
@@ -1,14 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const fetchFinancials = async (ticker: string) => {
-    try {
-      const response = await axios.get(
-        `http://localhost:5000/api/polygon/financials/${ticker}`
-      );
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/api/polygon/financials/${ticker}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export default { fetchFinancials };
+const fetchMovingAverages = async (ticker: string, window: string) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/api/polygon/movingAverage/${ticker}/${window}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default { fetchFinancials, fetchMovingAverages };
