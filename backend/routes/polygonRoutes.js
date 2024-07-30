55bae15b61ba9d1c15fe88422259d7d345001856
squilliam34 <wdfan0128@gmail.com>
2024-07-30 00:08:21 -0400

    added moving averages to routes

--- backend/routes/polygonRoutes.js
+++ backend/routes/polygonRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { fetchFinancials } = require("../controllers/financialsController");
+const {
+  fetchMovingAverages,
+} = require("../controllers/movingAveragesController");
 
 const router = express.Router();
 
 router.get("/financials/:ticker", fetchFinancials);
+router.get("/movingAverage/:ticker/:window", fetchMovingAverages);
 
 module.exports = router;
