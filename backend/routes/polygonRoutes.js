04abd6f47b737990ae5636c64f086b7767948550
squilliam34 <wdfan0128@gmail.com>
2024-07-31 01:01:09 -0400

    added route for company details

--- backend/routes/polygonRoutes.js
+++ backend/routes/polygonRoutes.js
@@ -3,10 +3,14 @@ const { fetchFinancials } = require("../controllers/financialsController");
 const {
   fetchMovingAverages,
 } = require("../controllers/movingAveragesController");
+const {
+  fetchCompanyDetails,
+} = require("../controllers/companyDetailController");
 
 const router = express.Router();
 
 router.get("/financials/:ticker", fetchFinancials);
 router.get("/movingAverage/:ticker/:window", fetchMovingAverages);
+router.get("/details/:ticker", fetchCompanyDetails);
 
 module.exports = router;
