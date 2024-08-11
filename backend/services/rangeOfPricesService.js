0ac09b6bcfbdce2c8cd102f494c85d9b81ae0eda
squilliam34 <wdfan0128@gmail.com>
2024-08-11 16:19:45 -0400

    updated services for backend

--- backend/services/rangeOfPricesService.js
+++ backend/services/rangeOfPricesService.js
@@ -2,9 +2,9 @@ const axios = require("axios");
 
 const API_KEY = process.env.POLYGON_API_KEY;
 
-const getRangeOfPrices = async (ticker, start, end) => {
+const getRangeOfPrices = async (ticker, start, end, period) => {
   try {
-    const BASE_URL = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/week/${start}/${end}?adjusted=true&sort=desc&apiKey=${API_KEY}`;
+    const BASE_URL = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/${period}/${start}/${end}?adjusted=true&sort=desc&apiKey=${API_KEY}`;
     const response = await axios.get(BASE_URL);
     return response.data;
   } catch (error) {
