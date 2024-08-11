dbdb2c645d598e0a76e996beae3570fa65aba5a2
squilliam34 <wdfan0128@gmail.com>
2024-08-11 16:10:03 -0400

    removed fields that api didn't hvae

--- frontend/src/utils/groupStockData.ts
+++ frontend/src/utils/groupStockData.ts
@@ -1,4 +1,4 @@
-import { Result } from '../types/interfaces';
+import { Result } from "../types/interfaces";
 
 interface GroupedData {
   [key: string]: { field: string; value: string | number }[];
@@ -6,28 +6,23 @@ interface GroupedData {
 
 export const groupStockData = (stock: Result): GroupedData => {
   return {
-    "Company Overview": [
-        { field: "Sector", value: stock.sector},
-        { field: "Dividend Yield", value: stock.dividend },
-    ],
+    "Company Overview": [],
     "Current Financials": [
-        { field: "Net Income", value: stock.netincome },
-        { field: "Total Revenue", value: stock.totalrevenue },
-        { field: "EBITDA", value: stock.ebitda },
+      { field: "Net Income", value: stock.netincome },
+      { field: "Total Revenue", value: stock.totalrevenue },
     ],
     Valuation: [
-        { field: "Price", value: stock.price },
-        { field: "PE Ratio", value: stock.peratio },
-        { field: "PEG Ratio", value: stock.pegratio },
-        { field: "PS Ratio", value: stock.psratio },
-        { field: "PB Ratio", value: stock.pbratio },
+      { field: "Price", value: stock.price },
+      { field: "PE Ratio", value: stock.peratio },
+      { field: "PEG Ratio", value: stock.pegratio },
+      { field: "PS Ratio", value: stock.psratio },
     ],
     "Moving Averages": [
-        { field: "10-day MA", value: stock.ma10 },
-        { field: "20-day MA", value: stock.ma20 },
-        { field: "30-day MA", value: stock.ma30 },
-        { field: "40-day MA", value: stock.ma40 },
-        { field: "50-day MA", value: stock.ma50 },
+      { field: "10-day MA", value: stock.ma10 },
+      { field: "20-day MA", value: stock.ma20 },
+      { field: "30-day MA", value: stock.ma30 },
+      { field: "40-day MA", value: stock.ma40 },
+      { field: "50-day MA", value: stock.ma50 },
     ],
   };
 };
